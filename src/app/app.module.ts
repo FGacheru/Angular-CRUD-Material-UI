0dd765ba038ed301b1200fd60dbad4096be295de
FGacheru <gacheruf12@gmail.com>
2023-10-26 16:00:07 +0300

    Add the dialog to add an employee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-
+import { AddEmployeeComponent } from './employee/add-employee/add-employee.component';
+import { UpdateEmployeeComponent } from './employee/update-employee/update-employee.component';
+import { DeleteEmployeeComponent } from './employee/delete-employee/delete-employee.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatRadioModule} from '@angular/material/radio';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEmployeeComponent,
+    UpdateEmployeeComponent,
+    DeleteEmployeeComponent
   ],
   imports: [
     BrowserModule,
@@ -19,7 +31,14 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/employee/add-employee/add-employee.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-employee',
+  templateUrl: './add-employee.component.html',
+  styleUrls: ['./add-employee.component.scss']
+})
+export class AddEmployeeComponent {
+  education: string[] = [
+    "Matric",
+    "Diploma",
+    "Intermidiate",
+    "Graduate",
+    "Post Graduate"
+  ]
+}
